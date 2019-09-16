92f9623b2cbef3a8c333112ac2c4567de4060231
yswift <yanli_uoh@163.com>
2019-09-16 21:36:02 +0800

    add constraint layout

--- app/src/main/java/cn/edu/uoh/learnandroid/DemoItem.java
+++ app/src/main/java/cn/edu/uoh/learnandroid/DemoItem.java
@@ -1,6 +1,7 @@
 package cn.edu.uoh.learnandroid;
 
 import cn.edu.uoh.learnandroid.activity.*;
+import cn.edu.uoh.learnandroid.ui.*;
 
 public class DemoItem {
     public static DemoItem[] items = {
@@ -8,6 +9,8 @@ public class DemoItem {
             new DemoItem("菜单", MenuActivity.class),
             new DemoItem("Intent", IntentActivity.class),
             new DemoItem("生命周期", LifeCycleActivity.class),
+            new DemoItem("Linear layout", LinearDemoActivity.class),
+            new DemoItem("Constraint layout", ConstraintDemoActivity.class),
     };
 
     String name;
