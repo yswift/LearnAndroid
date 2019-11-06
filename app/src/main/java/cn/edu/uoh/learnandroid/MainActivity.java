4dc4a3be5860b55a8f95f2c3c9bb3d9f8185ebed
yswift <yanli_uoh@163.com>
2019-11-06 17:20:03 +0800

    主菜单逆序

--- app/src/main/java/cn/edu/uoh/learnandroid/MainActivity.java
+++ app/src/main/java/cn/edu/uoh/learnandroid/MainActivity.java
@@ -10,6 +10,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     void initDemoList() {
         ListView demoListView = findViewById(R.id.demoList);
-        // 生成菜单数据
-        String[] titles = Arrays.stream(DemoItem.items).map(di -> di.title).toArray(String[]::new);
+        // 逆序
+        List<DemoItem> demoItemList = Stream.of(DemoItem.items).collect(Collectors.toList());
+        Collections.reverse(demoItemList);
+        // 取标题，生成菜单数据
+        String[] titles = demoItemList.stream().map(di -> di.title).toArray(String[]::new);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,titles);
         demoListView.setAdapter(adapter);
         // 处理单击事件
@@ -35,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
 //                Toast.makeText(MainActivity.this,"您点击了"+title,Toast.LENGTH_SHORT).show();
 
                 // 启动另一个Activity
-                Class target = DemoItem.items[position].targetActivity;
+                DemoItem item = demoItemList.get(position);
+                Class target = item.targetActivity;
                 Intent intent = new Intent(MainActivity.this, target);
                 startActivity(intent);
             }
